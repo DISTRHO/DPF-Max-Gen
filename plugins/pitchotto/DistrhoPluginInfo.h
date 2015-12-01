fbc831c0736e55c01d015b7cdfa661ff47f22fcc
ninodewit <ninodewit@outlook.com>
2015-12-01 16:04:03 +0100

    minor changes

--- plugins/pitchotto/DistrhoPluginInfo.h
+++ plugins/pitchotto/DistrhoPluginInfo.h
@@ -29,7 +29,7 @@
 #define DISTRHO_PLUGIN_LV2_CATEGORY "lv2:PitchPlugin"
 
 #define DISTRHO_PLUGIN_DESCRIPTION "Pitchotto is a pitch-shifter based on the \"Pitch-Shift\"-genpatch in Max, where Phase-shifting is used to achieve different intervals.\n\
-There are two shifted signals available, both with variable window-sizes for arpeggio-like sounds."
+There are two shifted signals available, both with variable delay-lengths for arpeggio-like sounds."
 #define DISTRHO_PLUGIN_VERSION d_cconst('D', 'M', 'p', 'c')
 
 #endif // DISTRHO_PLUGIN_INFO_H_INCLUDED
