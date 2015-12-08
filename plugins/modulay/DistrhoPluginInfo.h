db5f60eee0bfca798394ac2912bed1ca44828672
nino de wit <ninodewit@outlook.com>
2015-12-08 18:41:37 +0100

    shameless copyright namechange

--- plugins/modulay/DistrhoPluginInfo.h
+++ plugins/modulay/DistrhoPluginInfo.h
@@ -1,6 +1,5 @@
 /*
- * DPF Max Gen
- * Copyright (C) 2015 Filipe Coelho <falktx@falktx.com>
+ * Copyright (C) 2015 Nino de Wit <ninodig@hotmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any purpose with
  * or without fee is hereby granted, provided that the above copyright notice and this
